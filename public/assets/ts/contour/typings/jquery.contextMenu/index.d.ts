ce1f6b5dc378e3027400954c3b82922ba753dff7
jasekiw <jasongallavin@gmail.com>
2016-05-25 20:30:46 -0400

    add custom typings

---
+++ public/assets/ts/contour/typings/jquery.contextMenu/index.d.ts
@@ -0,0 +1,15 @@
+/**
+ * Created by Jason Gallavin on 5/25/2016.
+ */
+interface JQueryContextMenuRuntimeOptions
+{
+    $input : JQuery,
+    $label : JQuery,
+    $menu : JQuery,
+    $trigger : JQuery,
+    $callbacks : any[],
+    commands : any[]
+    inputs: JQuery,
+    hasTypes : JQuery,
+    ns : string
+}
